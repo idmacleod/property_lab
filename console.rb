1e6768295d55f2adabf8a4b9c216ef83db246403
idmacleod <idmacleod@gmail.com>
2020-04-14 17:13:01 +0100

    Readability tweaks

--- console.rb
+++ console.rb
@@ -5,17 +5,14 @@ Property.delete_all()
 
 prop1 = Property.new({"address" => "34 Whitby Avenue", "value" => 300000, "bedrooms" => 2, "year_built" => 1950})
 prop1.save()
-
 prop2 = Property.new({"address" => "2 Walrus Terrace", "value" => 250000, "bedrooms" => 1, "year_built" => 1800})
 prop2.save()
-
 prop3 = Property.new({"address" => "18 Llama Road", "value" => 10000000, "bedrooms" => 10, "year_built" => 1300})
 prop3.save()
-
 prop4 = Property.new({"address" => "0 Nexus of Reality", "value" => -42, "bedrooms" => 0, "year_built" => 42})
 prop4.save()
-
 puts "C -- 4 lines added to database!"
+
 puts "R -- Use all() method to read the table:"
 p Property.all()
 puts
